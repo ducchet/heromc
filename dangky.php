5f18ddc523da52057a2ccaba559526477c3ad421
Toru <112097745+ToruCC@users.noreply.github.com>
2022-12-15 22:33:34 +0700

    Add files via upload

---
+++ dangky.php
@@ -0,0 +1,99 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+		<link rel="icon" href="https://minefc.com/wp-content/themes/magicraft/favicon.png">
+	
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="client_id" content="1247393433-la5t1i7s8jc7ve74hbbacfih8q94t4of.apps.googleusercontent.com" />
+    	<meta name="google-signin-client_id" content="1247393433-la5t1i7s8jc7ve74hbbacfih8q94t4of.apps.googleusercontent.com" />
+    	<meta name="google-site-verification" content="DZbi7mtJx-M-oUHWhx_D3CztU9MEFaxVWPOKnf7hzmo" />
+		<meta property="fb:app_id" content="1089059977876558" /><link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon" />
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Thành Viên Minecraft</title>
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+        <link href="css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="css/bootstrap-datetimepicker.min.css" rel="stylesheet"/>
+        <link href="css/css.css" rel="stylesheet"/>
+        
+        <script src="js/jquery.min.js"></script>
+		<script src="js/function.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+
+
+<script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+
+     
+    </head>
+    <body>
+        <div class="container">
+            
+<div id="header">
+    <div class="row">
+        <div class="col-md-2 left-logo">
+            <div class="logo">
+                <a href="">
+                     <img src="https://heromc.net/wp-content/uploads/2019/11/Hero_title_small.png" class="img-responsive">
+                </a>
+            </div>
+        </div>
+                <div class="clear-header"></div>
+        <div class="col-md-8 middle-menu">
+            <ul id="main-menu" class="only-pc">
+				            </ul>
+        </div>
+    </div>
+</div>        
+		<div id="content">
+	
+<div class="row">
+<div class="col-md-12">
+        <div class="widget">
+            <h2 class="title">Đăng ký tài khoản</h2>
+                <div class="row">
+                    <div class="col-md-10">
+                    <form class="form-horizontal user-form" id="yw0" method="post"> 
+					<div class="form-group" id="thongbao">                        
+                    </div>
+					<div class="form-group">
+                            <label class="col-md-3 col-xs-12 control-label required" for="User_name">Tên tài khoản <span class="required">*</span></label>                   		    <div class="col-md-6 col-xs-12">
+                                <input class="form-control" type="text" value="" name="User_name" pattern="[A-Za-z0-9_]{3,16}" required title="Số kí tự cho phép 3 - 16 kí tự + Không có kí tự đặc biệt"  autocomplete="off" />                                                            </div>
+                        </div>
+					<div class="form-group">
+                            <label class="col-md-3 col-xs-12 control-label required" for="User_email">Email <span class="required">*</span></label>                   		    <div class="col-md-6 col-xs-12">
+                                <input class="form-control" name="User_email" id="User_email" type="text" maxlength="255">                                                            </div>
+                        </div>
+                        
+                        <div class="form-group">
+                            <label class="col-md-3 col-xs-12 control-label required" for="User_password">Mật khẩu <span class="required">*</span></label>                            <div class="col-md-6 col-xs-12">
+                                <input class="form-control" name="User_password" id="User_password" type="password" maxlength="255">                                                            </div>
+                        </div>
+                        
+                        <div class="form-group">
+                             <label class="col-md-3 col-xs-12 control-label required" for="User_repeatPassword">Xác nhận mật khẩu <span class="required">*</span></label>                            <div class="col-md-6 col-xs-12">
+                                <input class="form-control" name="User_repeatPassword" id="User_repeatPassword" type="password">                                                            </div>
+                        </div> 
+                       <div class="form-group">
+                             <label class="col-md-3 col-xs-12 control-label required" for="User_repeatPassword">Mã captcha <span class="required">*</span></label>                            <div class="col-md-6 col-xs-12">
+                               <div class="g-recaptcha" data-sitekey="6Ld6LbsUAAAAAF0MgujyaHEmfA-Yq8W0AdiPnlYH"></div>                                                      </div>
+                        </div>				
+                        <div class="form-group" style="padding-top: 6px;">
+                            <label class="col-md-3 col-xs-12 control-label"></label>
+                            <div class="col-md-8 col-xs-12 bt-padding">
+                                <input type="button" onclick="dangkyy();" class="btn btn-main" id="dangky" value="Đăng ký">
+                                <span></span>
+                                <!--<a href="index.php" class="btn">Đăng nhập</a>-->
+                                
+                            </div>
+                        </div>
+                    </form>                </div>
+                <div class="col-md-3"></div>
+            </div>
+        </div>
+    
+    </div>
+</div>
+</div>
+<!--?php include('footer.php'); ?>-->
